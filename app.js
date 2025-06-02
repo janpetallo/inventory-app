7b202976abaeda03416e71237ab3e70261caec22
janpetallo <101074771+janpetallo@users.noreply.github.com>
2025-06-02 15:20:09 -0600

    App setup

---
+++ app.js
@@ -0,0 +1,15 @@
+
+const express = require('express');
+const app = express(); // Import the express module to create an Express application
+
+const path = require('path'); // Import the path module to handle file paths
+
+app.set("views", path.join(__dirname, "views"));
+app.set('view engine', 'ejs'); // Set the view engine to EJS
+
+app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded bodies
+const assetsPath = path.join(__dirname, 'assets'); 
+app.use('/assets', express.static(assetsPath)); // Serve static files from the 'assets' directory
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Express app listening on port ${PORT}!`));
