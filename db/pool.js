cab13b370227e83bd7d3389d84d27e631aacead8
janpetallo <101074771+janpetallo@users.noreply.github.com>
2025-06-02 15:46:08 -0600

    Db connection pool implementation

---
+++ db/pool.js
@@ -0,0 +1,9 @@
+const {Pool} = require('pg');
+
+module.exports = new Pool({
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: 5432
+});
