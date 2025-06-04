f70e32f5283ba79edf93fd0bf03b4282a8a6abec
janpetallo <101074771+janpetallo@users.noreply.github.com>
2025-06-03 20:16:25 -0600

    Views: Arists initial implementation

--- controllers/artistsController.js
+++ controllers/artistsController.js
@@ -3,7 +3,11 @@
 
 async function getArtistsList(req, res) {
     console.log("Fetching artists list...");
-    res.send("Artists list fetched successfully. - WIP");
+    // Here you would typically query the database to get the list of artists.
+    res.render('artists', {
+        title: 'Artists',
+        artists: [] // This should be replaced with actual categories data from the database
+    });
 }
 
 
--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -3,6 +3,7 @@
 
 async function getCategoriesList(req, res) {
     console.log("Fetching categories list...");
+    // Here you would typically fetch the categories from the database
     res.render('categories', {
         title: 'Categories',
         categories: [] // This should be replaced with actual categories data from the database
