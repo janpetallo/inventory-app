6a5c6994a8b2650e134c563e1401f25da14b0a93
janpetallo <101074771+janpetallo@users.noreply.github.com>
2025-06-04 18:58:27 -0600

    Feature: Home page improvements to show details

--- db/queries.js
+++ db/queries.js
@@ -1,7 +1,20 @@
 const pool = require('./pool');
 
 async function getAllVinyls() {
-    const query = 'SELECT * FROM vinyls';
+    const query = `
+    SELECT 
+        vinyls.title, 
+        artists.name AS artist_name, 
+        genres.name AS genre_name, 
+        vinyls.release_year, 
+        vinyls.stock_quantity, 
+        vinyls.price, 
+        vinyls.cover_image
+    FROM vinyls
+    JOIN artists ON vinyls.artist_id = artists.id
+    JOIN genres ON vinyls.genre_id = genres.id
+    ORDER BY vinyls.title
+    `;
     const { rows } = await pool.query(query);
     return rows;
 }
