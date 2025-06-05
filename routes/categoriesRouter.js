7cb49bc054be12028bf3d0db7a6b160c2ce06386
janpetallo <101074771+janpetallo@users.noreply.github.com>
2025-06-04 18:19:04 -0600

    Route: added get categories by id

--- routes/categoriesRouter.js
+++ routes/categoriesRouter.js
@@ -4,7 +4,7 @@ const categoriesRouter = Router();
 const categoriesController = require('../controllers/categoriesController');
 
 categoriesRouter.get('/', categoriesController.getCategoriesList); // Route to get the list of categories
-
+categoriesRouter.get('/:name/:id', categoriesController.getVinylsByCategory); // Route to get vinyls by category ID
 
 
 module.exports = categoriesRouter;
